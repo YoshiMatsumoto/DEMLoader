e14bc9310249691af993c960b729c5f2aa07e3cc
YoshiMatsumoto <yos.mtmt@gmail.com>
2018-11-11 21:21:00 +0900

    Update CsvToPoint.py

--- CsvToPoint.py
+++ CsvToPoint.py
@@ -4,24 +4,24 @@ import math
 import rhinoscriptsyntax as rs
 
 def ReadPointsDef():
+    crvList =[]
     filter = "csv file (*.csv)|*.csv|All Files (*.*)|*.*||"
-    filename = rs.OpenFileName("Open Point File", filter)
-    if not filename: return
-    
-    with open(filename, "r") as f:
-        contents = f.readlines()
-
-    points=[]
-    for text in contents:
-        items = text.strip("()\n").split(",")    
-        if len(items)==3:
-            x = float(items[0])
-            y = float(items[1])
-            z = float(items[2])
-            rs.AddPoint(x,z,y)
-
-    #contents = [__point_from_string(line) for line in contents]
-    return points
+    fileList = rs.OpenFileNames("Open Point File", filter)
+    if not fileList: return
+    for filename in fileList:
+            with open(filename, "r") as f:
+                contents = list(f.readlines())
+                for text in contents:
+                    pts = []
+                    items = text.strip("()\n").split(",")    
+                    if len(items)==3:
+                        x = float(items[0])
+                        y = float(items[1])
+                        z = float(items[2])
+                        pt = rs.AddPoint(x,z,y)
+                        pts.append(pt)
+                crvList = rs.AddInterpCurve(pts)
+    return crvList
 
 if( __name__ == "__main__" ):
     ReadPointsDef()
